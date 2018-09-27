1b112512aa6b9ce6e5ec5aba43b2e056e6ef5b17
Gui Guy <jedandeganwang1999@gmail.com>
2018-09-27 14:30:32 -0500

    Create the repository

---
+++ RoomClient/src/roomclient/RoomClientMain.java
@@ -0,0 +1,14 @@
+package roomclient;
+
+/**
+ * The main class for this client
+ * @author Jed Wang
+ */
+public class RoomClientMain {
+    /**
+     * The main method
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ RoomServer/src/roomserver/RoomServerMain.java
@@ -0,0 +1,14 @@
+package roomserver;
+
+/**
+ * The main class for this server
+ * @author Jed Wang
+ */
+public class RoomServerMain {
+    /**
+     * The main method
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
